17e6cdc54064f9664699b4ea1a3426462e1e9c5f
Alex MacLure <almaclure@gmail.com>
2021-08-22 19:39:49 +0000

    Update register cookie function

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (db) => {
+const { getUserByEmail} = require('../helpers');
 
+const registerGet = function (db) {
   router.get("/", (req, res) => {
 
     const templateVars = {
@@ -21,4 +22,59 @@ module.exports = (db) => {
     res.render('register', templateVars);
   });
   return router;
-};
+}
+
+//Use module export instead..
+const users = {
+  "userA": {
+    id: "idUserA",
+    email: "a@a.com",
+    password: "a"
+  },
+ "userB": {
+    id: "idUserB",
+    email: "b@b.com",
+    password: "b"
+  }
+}
+
+//!!!Need to update with users from DB as opposed to users object!!!///
+const registerPost = function (db) {
+  router.post("/", (req, res) => {
+    //Grab email and password entered into form
+    const email = req.body['email'];
+    const password = req.body['password'];
+    const newUser = "UserC" //TEMPORARY
+    const id = "id" + newUser; //TEMPORARY
+
+
+    // const id = generateRandomString(); //Creates a unique id for the new user
+
+    //Display error if email is blank or password is blank
+    if (!email || !password) {
+      //Send status code and display error message with one line:
+      return res.status(400).send(`Not a valid email or password. Please <a href='/register'>try again</a>`);
+    }
+    //Display error if email is already in user "database"
+    if (getUserByEmail(email, users)) {
+      //Send status code and display error message with one line:
+      return res.status(400).send(`Email already exists. Please <a href='/login'>login</a>`);
+    }
+    //TEMPORARY Adds the new created user to the users object
+    users[newUser] = {
+      id,
+      email,
+      password
+    };
+
+    console.log(users);
+
+    //Sets a user_id cookie to the newly created id
+    res.cookie('user_id', id); //Might be an issue if there is already a cookie? Maybe overwrites it?
+    res.redirect('/profile');
+  })
+  return router;
+}
+
+//Exports for use in server.js
+module.exports = {registerGet, registerPost};
