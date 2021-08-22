6b882d1b8739ee3bc70875ee38d5bfec37d7e18f
Alex MacLure <almaclure@gmail.com>
2021-08-22 18:04:52 +0000

    Update login functionality with unsecure cookie

--- helpers.js
+++ helpers.js
@@ -2,27 +2,18 @@
 const getUserByEmail = function(email, database) {
   for (const user in database) {
     if (email === database[user]['email']) { //update this to actually use DB as opposed to user object
-      return user;
+      return database[user];
     }
   }
 };
 
 //Get user password from "database"
 const getUserPassword = function(user, database) {
-  if (database[user]) {
-    return database[user]['password']; //update this to actually use DB as opposed to user object
+  if (user) {
+    return user['password']; //update this to actually use DB as opposed to user object
   }
   console.log("User not in database");
   return null;
 };
 
-//Return user_id from user "database" via email lookup
-const  getUserID = function(user, database) {
-  if (database[user]) {
-    return database[user]['id']; //update this to actually use DB as opposed to user object
-  }
-  console.log("User not in database");
-  return null;
-};
-
-module.exports = { getUserByEmail, getUserPassword, getUserID };
+module.exports = { getUserByEmail, getUserPassword};
