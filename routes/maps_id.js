9e19906ad00a96772b5ecd62783b7db0cd4e8d46
Michael <michael.dburke56@gmail.com>
2021-08-23 13:54:06 -0400

    fix templatevars issue on index.

--- routes/maps_id.js
+++ routes/maps_id.js
@@ -8,19 +8,17 @@ module.exports = (db) => {
     const userId = req.cookies.user_id;
     const mapId = req.params.id
     const templateVars = {
-<<<<<<< HEAD
       userId,
       mapId
-    };
+    }
+
 
     ////////
     //query db for spefic map information
     ///////
 
-=======
-      mapId: req.params.id,
-    }
->>>>>>> 7af310da4ab927f2998dbde1c10eb51e27875f10
+
+
     // db.query(`SELECT * FROM users;`)
     //   .then(data => {
     //     const users = data.rows;
