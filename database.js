8781e1c4b8a5711474a26e75fff1ed57b357cc03
Michael <michael.dburke56@gmail.com>
2021-08-23 20:16:23 -0400

    Profile render templateVars.

--- database.js
+++ database.js
@@ -1,108 +1,148 @@
-const getUserWithEmail = function(email) {
+const getUserWithEmail = function (email) {
   //Define query
   const queryString = `
   SELECT *
   FROM users
   WHERE email = $1
-  `
+  `;
   //Return promise for query
-  return pool
-    .query(queryString, [email])
-    .then((result) => {
-      //If result is not found inside DB, return null
-      if (result.rows.length === 0) {
-        return null;
-      }
-      //If result is found, return the object for the user
-      return result.rows[0];
-    })
-    //Console log error if can't connect to DB
-    .catch((err) => {
-      console.log(err.message);
-    });
+  return (
+    pool
+      .query(queryString, [email])
+      .then((result) => {
+        //If result is not found inside DB, return null
+        if (result.rows.length === 0) {
+          return null;
+        }
+        //If result is found, return the object for the user
+        return result.rows[0];
+      })
+      //Console log error if can't connect to DB
+      .catch((err) => {
+        console.log(err.message);
+      })
+  );
 };
 
-const getUserWithId = function(id, db) {
+const getUserWithId = function (id, db) {
   //Define query
   const queryString = `
   SELECT *
   FROM users
   WHERE id = $1
-  `
+  `;
   //Return promise for query
 
   // return pool GET RID OF?
 
-   return db.query(queryString, [id])
-    .then((result) => {
+  return (
+    db
+      .query(queryString, [id])
+      .then((result) => {
+        // console.log(result.rows[0])
 
-      // console.log(result.rows[0])
-
-      //If result is not found inside DB, return null
-      if (result.rows.length === 0) {
-        return null;
-      }
-      //If result is found, return the object for the user
-      return result.rows[0];
-    })
-    //Console log error if can't connect to DB
-    .catch((err) => {
-      console.log(err.message);
-    });
+        //If result is not found inside DB, return null
+        if (result.rows.length === 0) {
+          return null;
+        }
+        //If result is found, return the object for the user
+        return result.rows[0];
+      })
+      //Console log error if can't connect to DB
+      .catch((err) => {
+        console.log(err.message);
+      })
+  );
 };
 
-const getParksWithCreatorId = function(id, db) {
-
+const getParksWithCreatorId = function (id, db) {
   //Define query
   const queryString = `
   SELECT parks.id as park_id, park_name, creator_id, users.name as creator_name, coordinates_long, coordinates_lat
   FROM parks
   JOIN users ON users.id = creator_id
   WHERE creator_id = $1;
-  `
-   return db.query(queryString, [id])
-    .then((result) => {
+  `;
+  return (
+    db
+      .query(queryString, [id])
+      .then((result) => {
+        // console.log(result.rows[0])
 
-      // console.log(result.rows[0])
-
-      //If result is not found inside DB, return null
-      if (result.rows.length === 0) {
-        return null;
-      }
-      //If result is found, return the object for the user
-      return result.rows;
-    })
-    //Console log error if can't connect to DB
-    .catch((err) => {
-      console.log(err.message);
-    });
+        //If result is not found inside DB, return null
+        if (result.rows.length === 0) {
+          return null;
+        }
+        //If result is found, return the object for the user
+        return result.rows;
+      })
+      //Console log error if can't connect to DB
+      .catch((err) => {
+        console.log(err.message);
+      })
+  );
 };
 
-const getParksWithMapId = function(id, db) {
-
+const getParksWithMapId = function (id, db) {
   //Define query
   const queryString = `
   SELECT parks.id as parks_id, park_name, maps.id as maps_id, maps.name as maps_name, coordinates_long, coordinates_lat
   FROM parks
   JOIN maps ON maps.id = map_id
   WHERE maps.id = $1;
-  `
-   return db.query(queryString, [id])
-    .then((result) => {
+  `;
+  return (
+    db
+      .query(queryString, [id])
+      .then((result) => {
+        // console.log(result.rows[0])
 
-      // console.log(result.rows[0])
-
-      //If result is not found inside DB, return null
-      if (result.rows.length === 0) {
-        return null;
-      }
-      //If result is found, return the object for the user
-      return result.rows;
-    })
-    //Console log error if can't connect to DB
-    .catch((err) => {
-      console.log(err.message);
-    });
+        //If result is not found inside DB, return null
+        if (result.rows.length === 0) {
+          return null;
+        }
+        //If result is found, return the object for the user
+        return result.rows;
+      })
+      //Console log error if can't connect to DB
+      .catch((err) => {
+        console.log(err.message);
+      })
+  );
 };
 
-module.exports = { getUserWithEmail, getUserWithId, getParksWithCreatorId, getParksWithMapId};
+const getMapsWithCreatorId = function (id, db) {
+  //Define query
+  const queryString = `
+  SELECT maps.id as map_id, maps.name as maps_name, creator_id, description, users.name as creator_name
+  FROM maps
+  JOIN users ON users.id = creator_id
+  WHERE creator_id = $1;
+
+  `;
+  return (
+    db
+      .query(queryString, [id])
+      .then((result) => {
+
+        //If result is not found inside DB, return null
+        if (result.rows.length === 0) {
+          return null;
+        }
+        //If result is found, return the object for the user
+        return result.rows;
+      })
+      //Console log error if can't connect to DB
+      .catch((err) => {
+        console.log(err.message);
+      })
+  );
+};
+
+module.exports = {
+  getUserWithEmail,
+  getUserWithId,
+  getParksWithCreatorId,
+  getParksWithMapId,
+  getMapsWithCreatorId,
+};
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getParksWithCreatorId} = require('../database.js');
+const { getParksWithCreatorId, getMapsWithCreatorId } = require('../database.js');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -11,29 +11,54 @@ module.exports = (db) => {
     if (!userId) {
       return res.redirect("maps");
     }
-    const templateVars = {
-      userId,
-    };
 
-    //Call function from database.js file
-    getParksWithCreatorId(userId, db)
-    .then((result) => {
 
-      console.log(result);
 
-      //Store array from query in templateVars
+    Promise.all([getMapsWithCreatorId(userId, db), getParksWithCreatorId(userId, db)]).then((values) => {
+      console.log("is this working???")
+      console.log("values[0]:  ", values[0]);
+      console.log("values[1]:  ", values[1]);
+
       const templateVars = {
         userId,
-        parksArray: result
-      };
+        parksArray : values[1],
+        mapsArray : values[0]
+      }
+      res.render('user_profile', templateVars);
+    });
 
-      //Pass to user_profile
-      res.render("user_profile", templateVars);
-      return result;
+  //   .then((result) => {
+
+  // let mapRes = [];
+
+  //   getMapsWithCreatorId(userId, db)
+  //   .then((result) => {
+
+
+
+  //     // templateVars['mapsArray'] = result;
+  //     mapRes = result;
+  //     return result;
+
+  //   })
+  //   .then
+  //   (getParksWithCreatorId(userId, db))
+  //   .then((result) => {
+
+
+
+  //     //Store array from query in templateVars
+  //       const templateVars = {
+  //         userId,
+  //         parksArray : result,
+  //         mapsArray : mapRes
+  //       }
+  //     res.render("user_profile", templateVars);
+  //     return result;
 
       //we have the parks information but we need the maps information now.
     });
-  });
-
+  // });
+// });
   return router;
 };
