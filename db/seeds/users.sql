20b3dfe4eb8d76258b70b31a00db14c996cbb875
Michael <michael.dburke56@gmail.com>
2021-08-26 20:52:19 -0400

    Update seeds.

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,2 +1,7 @@
+insert into users (name, email, password) values ( 'Mike B', 'mike@bemail.com', 'password');
+
+insert into users (name, email, password) values ( 'Alex M', 'alex@email.com', 'password');
+
+insert into users (name, email, password) values ( 'Burhan S', 'Burhan@email.com', 'password');
+
 insert into users (name, email, password) values ( 'Constantia Lachaize', 'clachaize3@cdbaby.com', 'QZSzqaq8iY');
-insert into users (name, email, password) values ( 'Marcy McShirrie', 'mmcshirrie4@blogger.com', '4aOdUDx');
