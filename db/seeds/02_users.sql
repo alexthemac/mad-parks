48925e4c617d10f7fa2ab0af61615689dd8fd4dc
Alex MacLure <almaclure@gmail.com>
2021-11-23 23:55:12 +0000

    Update README and fix database dropping and seeding

---
+++ db/seeds/02_users.sql
@@ -0,0 +1,7 @@
+insert into users (name, email, password) values ( 'Burhan S', 'burhan@email.com', 'password');
+
+insert into users (name, email, password) values ( 'Mike B', 'mike@email.com', 'password');
+
+insert into users (name, email, password) values ( 'Alex M', 'alex@email.com', 'password');
+
+insert into users (name, email, password) values ( 'Constantia Lachaize', 'clachaize3@cdbaby.com', 'QZSzqaq8iY');
